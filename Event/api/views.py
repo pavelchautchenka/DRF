759b6eba70d9742048be51c0f6e07a7378f4bc4a
pavel shevchenko <litvin.para@gmail.com>
2024-02-16 12:05:37 +0100

    HM 34-35

--- Event/api/views.py
+++ Event/api/views.py
@@ -1,11 +1,8 @@
 from django.utils import timezone
-from rest_framework import viewsets
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import ListCreateAPIView, ListAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.core.cache import cache
 from Event.cache import get_cached
 from Event.models import Event, User
 from .serializers import EventSerializer, UserSerializer
